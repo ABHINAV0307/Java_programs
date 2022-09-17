09ab39d9e73e507089ae06a50ee4a67f2fe4cab4
Abhinav Tripathi <83867425+ABHINAV0307@users.noreply.github.com>
2022-09-18 00:33:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.abhi;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args){
+    //basic output
+    System.out.println("Hello World");
+    //basic input
+    Scanner input=new Scanner(System.in);
+    System.out.println(input.nextInt());
+    System.out.println((input.next()));
+}
+}
+
