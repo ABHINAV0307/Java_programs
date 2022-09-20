2eee5a72468da210a8c16d38150e6e2e5703ad78
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-09-20 20:32:09 +0530

    upd

--- funtion/armstrong.java
+++ funtion/armstrong.java
@@ -6,13 +6,25 @@ public class armstrong {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        cal(a);
+        System.out.println(cal(a)+"\n\n");
+        System.out.println("Three digit Armstrong Number");
+        for(int i=100;i<1000;i++){
+            if(cal(i))
+            System.out.print(i);
+        }
     }
-
-    static void cal(int num){
+//three digit Armstrong
+    static boolean cal(int num){
         int rem=0,sum=0;
-        rem=num%10;
-        sum+=rem*rem*rem;
-    num=num/10;
+        int dup=num;
+       while(num>0) {
+           rem = num % 10;
+           sum += rem * rem * rem;
+           num = num / 10;
+       }
+       if(sum==dup)
+        return true;
+       else
+           return false;
     }
 }
