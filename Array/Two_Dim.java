47148c1d236549be0534b2dc0a8f62d55a7945cf
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-09-22 00:57:30 +0530

    upg

---
+++ Array/Two_Dim.java
@@ -0,0 +1,11 @@
+package com.abhi.Array;
+
+public class Two_Dim {
+    public static void main(String[] args) {
+        int [][]a=new int[3][];
+        int [][] a2 ={
+                {1,2,3},
+        {1,4,5},
+        {6,2,5}};
+    }
+}
