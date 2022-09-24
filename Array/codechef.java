097fbaf632cd50ce6b7bffa7cda7749950b98009
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-09-24 11:25:05 +0530

    k

---
+++ Array/codechef.java
@@ -0,0 +1,11 @@
+package com.abhi.Array;
+
+import java.util.Scanner;
+
+public class codechef {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+    }
+}
