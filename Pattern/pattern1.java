e675612853ee44b89454e1dca2fe45bfe89dd874
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-10-25 08:14:02 +0530

    new

--- Pattern/pattern1.java
+++ Pattern/pattern1.java
@@ -2,11 +2,14 @@ package com.abhi.Pattern;
 
 public class pattern1 {
     public static void main(String[] args) {
-
+pattern(4);
     }
     static void pattern(int n){
         for (int i=0;i<n;i++){
-
+        for(int j=4;j>i;j--){
+            System.out.print("*");
+        }
+            System.out.println("");
         }
     }
 }
