225cef3552a02439535a76aa8db180f6bcf4ad0d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-11-09 22:44:51 +0530

    pattern

--- Pattern/pattern.java
+++ Pattern/pattern.java
@@ -1,10 +1,23 @@
 package com.abhi.Pattern;
+import java.lang.Math;
 
 public class pattern {
     public static void main(String[] args) {
         patt(4);
     }
-     public static void patt(int a){
+    public static void patt(int n){
+    for(int row=0;row<2*n;row++){
+    int nocol=row>n ?2*n-row : row;
 
+    int space=(n-nocol);
+    for(int i=0;i<space;i++){
+        System.out.print(" ");
+    }
+
+    for(int col=0;col<nocol;col++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
     }
 }
