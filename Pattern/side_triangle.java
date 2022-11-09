bf291d284511c06a40e9e29058541505ad990229
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-11-09 18:28:18 +0530

    w

--- Pattern/side_triangle.java
+++ Pattern/side_triangle.java
@@ -8,6 +8,7 @@ public class side_triangle {
         for (int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
                 System.out.print("  ");
+        
             }
             System.out.println("*");
         }
