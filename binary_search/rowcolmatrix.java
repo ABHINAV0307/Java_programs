21b5365704e1678dd67b17fefd0555b23a81654d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-11-16 00:54:32 +0530

    h

---
+++ binary_search/rowcolmatrix.java
@@ -0,0 +1,19 @@
+package com.abhi.binary_search;
+
+import java.util.Arrays;
+
+public class rowcolmatrix {
+    public static void main(String[] args) {
+int [] [] arr={
+        {},
+        {},
+        {}
+};
+        System.out.println(Arrays.toString(search(arr,)));
+    }
+    static int[] search(int [][] matrix, int traget){
+        int r=0;
+        int c= matrix.length-1;
+
+    }
+}
