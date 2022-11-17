6fc5ffd06d5b43910b4ce6d7ffda2ea3d748ffd1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-11-17 16:37:04 +0530

    ok

---
+++ EPAM/cycleswap.java
@@ -0,0 +1,14 @@
+package com.abhi.EPAM;
+
+public class cycleswap {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        cycle(arr,3);
+    }
+    static void main(int []arr,int k){
+        if(k==arr.length)
+        for(int i=k;i<arr.length;i++){
+            cout
+        }
+    }
+}
--- EPAM/pizza.java
+++ EPAM/pizza.java
@@ -1,7 +1,19 @@
 package com.abhi.EPAM;
 
+import java.util.Scanner;
+
 public class pizza {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int k=0;
+
+        int b=sc.nextInt();
+        int a=sc.nextInt();
+        int val=a*b;
+        for(int i=a;i<val;i=i+8){
+            if(b%i==0)
+                k++;
+        }
+        System.out.println(k);
     }
 }
