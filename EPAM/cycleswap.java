7fe5c7961525902c70ff6bac0126906a01590774
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-11-23 22:27:42 +0530

    k

--- EPAM/cycleswap.java
+++ EPAM/cycleswap.java
@@ -5,10 +5,10 @@ public class cycleswap {
         int [] arr={1,2,3,4,5};
         cycle(arr,3);
     }
-    static void main(int []arr,int k){
+    static void cycle(int []arr,int k){
         if(k==arr.length)
-        for(int i=k;i<arr.length;i++){
-            cout
+        for(int i=k;i<arr.length;i++) {
+
         }
     }
 }
