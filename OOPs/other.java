004c4f3e03f5a1387ca15d70d0bfb78e93913e7a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-08 00:10:16 +0530

    Create other.java

---
+++ OOPs/other.java
@@ -0,0 +1,24 @@
+package com.abhi.OOPs;
+
+public class other {
+    public static void main(String[] args) {
+        Student abhi=new Student();
+    Student random=new Student(abhi);
+        System.out.println(random.name);
+    }
+static class Student {
+        int rno;
+        String name;
+
+        Student (Student other){
+            this.name=other.name;
+            this.rno=other.rno;
+        }
+
+    public Student() {
+             this.rno=10;
+            this.name="aBhi";
+    }
+}
+}
+
