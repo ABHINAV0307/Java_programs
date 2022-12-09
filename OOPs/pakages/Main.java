7e8faf1474efd29f35152859a72fefcb88e881c1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-09 11:18:32 +0530

    Create Main.java

---
+++ OOPs/pakages/Main.java
@@ -0,0 +1,14 @@
+package com.abhi.OOPs.pakages;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        human abhi = new human(22, "abhinav", 1200000, false);
+        human rahul = new human(22, "rahul", 120042, true);
+        System.out.println(abhi.population);
+        System.out.println(rahul.population);
+    }
+}
