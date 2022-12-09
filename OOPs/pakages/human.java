71466fd93f6e69545a7cfc2b0a652d2a92c41cad
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-09 19:06:35 +0530

    Update human.java

--- OOPs/pakages/human.java
+++ OOPs/pakages/human.java
@@ -5,13 +5,14 @@ public class human {
    String name;
    int salary;
    Boolean married;
-   long population;
+   static long population;
    public human(int age , String name ,int salary,Boolean married){
        this.age=age;
        this.name=name;
        this.salary=salary;
        this.married=married;
-this.population+=1;
+        human.population+=1;
+        //to access static variable you need to use class name
    }
 }
 
