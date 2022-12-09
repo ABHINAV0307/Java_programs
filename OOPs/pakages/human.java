48dca4ec30958a31ff81055a7710194ebbcf4e8a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-09 11:18:25 +0530

    Create human.java

---
+++ OOPs/pakages/human.java
@@ -0,0 +1,17 @@
+package com.abhi.OOPs.pakages;
+
+public class human {
+   int age;
+   String name;
+   int salary;
+   Boolean married;
+   long population;
+   public human(int age , String name ,int salary,Boolean married){
+       this.age=age;
+       this.name=name;
+       this.salary=salary;
+       this.married=married;
+this.population+=1;
+   }
+}
+
