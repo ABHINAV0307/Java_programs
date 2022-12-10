783d5b9c886b067b43d00c583bd0cff3225214a2
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-10 10:36:41 +0530

    Update innerclasses.java

--- OOPs/innerclasses.java
+++ OOPs/innerclasses.java
@@ -2,3 +2,9 @@ class test{
     static String name;
     public test(String name){
         test.name=name;
+public class innerclasses {
+        test a=new test("abhi");
+        System.out.println(a.name);
+    }
+
+}
