a6251e546a49d976a693a117c363bf6171c78494
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-10 22:25:45 +0530

    Create try1.java

---
+++ OOPs/pakages/try1.java
@@ -0,0 +1,9 @@
+
+public class try1 {
+    public static void main(String[] args) {
+//        human abhi = new human(22, "abhinav", 1200000, false);
+//        human rahul = new human(22, "rahul", 120042, true);
+        System.out.println(human.population);
+        System.out.println(human.population);
+        //static variables don't require object creation for calling
+    }
