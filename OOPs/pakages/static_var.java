7911a8cf3ee2eb962f798d001f692a916a6102c9
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-10 22:26:15 +0530

    Create static_var.java

---
+++ OOPs/pakages/static_var.java
@@ -0,0 +1,13 @@
+
+public class static_var {
+    public static void main(String[] args) {
+
+    }
+    static void fun(){
+ static_var obj=new static_var();
+ obj.greeting();
+ //you cant access non static stuff without referencing their instances in
+ //a static context
+    }
+    void greeting(){
+        fun();
