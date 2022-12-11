749554fde482f4438b2480faf0959a9e7db57cd1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-11 21:57:32 +0530

    Update main.java

--- singleton/main.java
+++ singleton/main.java
@@ -1,5 +1,7 @@
 package com.abhi.singleton;
 
+public class main {
+    public static void main(String[] args) {
         singleton obj1=singleton.getInstance();
         singleton obj2=singleton.getInstance();
         singleton obj3=singleton.getInstance();
