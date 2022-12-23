76940b4daa2cb495580bae28c56966c02a1dbd7a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-23 14:31:03 +0530

    Update car.java

--- OOPs/interfaces/car.java
+++ OOPs/interfaces/car.java
@@ -3,6 +3,8 @@ package com.abhi.OOPs.interfaces;
 public class car implements engine,brake{
     public void start() {
         System.out.println("I start like normal car");
+    }
+
     public void stop() {
         System.out.println("I stop like normal car");
 }
