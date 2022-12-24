375b9feef28fe0497e7bd085f18b854fa042fde6
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-24 11:38:36 +0530

    b

--- OOPs/interfaces/cdplayer.java
+++ OOPs/interfaces/cdplayer.java
@@ -1,2 +1,13 @@
 package com.abhi.OOPs.interfaces;
 
+public class cdplayer implements media {
+    @Override
+    public void start() {
+        System.out.println("Start music");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop music");
+    }
+}
