4bda413834d4d1ab01b4a1d4e72c41fbfdd0834c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-26 20:34:14 +0530

    Update electric_engine.java

--- OOPs/interfaces/electric_engine.java
+++ OOPs/interfaces/electric_engine.java
@@ -4,6 +4,8 @@
     @Override
     public void stop() {
         System.out.println("Electric engine stop");
+
+    }
     public void acc() {
         System.out.println("Electric engine accelarate");
 
