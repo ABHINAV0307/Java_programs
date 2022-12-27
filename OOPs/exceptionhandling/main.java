0847e74ca3291a4fc4ed1a49fc377316d1e12b47
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-27 21:22:37 +0530

    Create main.java

---
+++ OOPs/exceptionhandling/main.java
@@ -0,0 +1,11 @@
+package com.abhi.OOPs.exceptionhandling;
+
+public class main {
+    public static void main(String[] args) {
+        int a=5;
+        int b=0;
+       try {
+           divide(a,b);
+
+           String name="Abhinav";
+           if(name.equals("Abhinav")){
