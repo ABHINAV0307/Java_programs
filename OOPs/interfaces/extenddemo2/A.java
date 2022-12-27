d0a07b6a61068ec8fcde3e94d104033ad0b5d9d8
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-27 06:54:38 +0530

    Update A.java

--- OOPs/interfaces/extenddemo2/A.java
+++ OOPs/interfaces/extenddemo2/A.java
@@ -2,4 +2,5 @@ package com.abhi.OOPs.interfaces.extenddemo2;
 
 public interface  A {
     //static interface method should always have a body
+    //call via the interface name
     static void greeting(){
