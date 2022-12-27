f64e0006da20854f5b844cd6244923464d184930
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-27 06:54:52 +0530

    Update A.java

--- OOPs/interfaces/extenddemo2/A.java
+++ OOPs/interfaces/extenddemo2/A.java
@@ -4,6 +4,7 @@ public interface  A {
     //static interface method should always have a body
     //call via the interface name
     static void greeting(){
+        System.out.println("Hey I am static");
     }
     default void fun(){
         System.out.println("I m in A");
