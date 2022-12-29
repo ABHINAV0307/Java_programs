fe1fad60a6e428fbef4419843e904c3a301561a0
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-29 21:44:39 +0530

    Create main.java

---
+++ OOPs/collection/main.java
@@ -0,0 +1,9 @@
+package com.abhi.OOPs.collection;
+public class main {
+    public static void main(String[] args) {
+        List<Integer> v=new Vector<>();
+        v.add(45);
+        v.add(05);
+        v.add(35);
+        System.out.println(v);
+}
