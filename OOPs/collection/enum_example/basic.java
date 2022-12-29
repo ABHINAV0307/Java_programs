f56bebaa5f5336732911361cafe6a5ebd319c1cc
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-29 21:44:13 +0530

    Create basic.java

---
+++ OOPs/collection/enum_example/basic.java
@@ -0,0 +1,12 @@
+        @Override
+        public void hello() {
+            System.out.println("how are you");
+        }
+    }
+
+    public static void main(String[] args) {
+        week w;
+        w=week.mon;
+        w.hello();
+    }
+}
