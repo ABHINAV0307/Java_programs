899f13ecc9b245ace4a4bddb0a56d4f242a1e40a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-29 22:38:40 +0530

    Update A.java

--- OOPs/interfaces/nested/A.java
+++ OOPs/interfaces/nested/A.java
@@ -1,6 +1,13 @@
 package com.abhi.OOPs.interfaces.nested;
 
 public class A {
+    //nested interface
+    public interface nestedinterface{
+        boolean isodd(int num);
+    }
+}
+class B implements A.nestedinterface{
+
     @Override
     }
 }
