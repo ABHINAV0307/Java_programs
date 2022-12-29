050b4ab744d64654da847f100fc0a6a4e9afe79c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-29 21:44:28 +0530

    Update basic.java

--- OOPs/collection/enum_example/basic.java
+++ OOPs/collection/enum_example/basic.java
@@ -3,6 +3,9 @@ package com.abhi.OOPs.collection.enum_example;
 public class basic {
     enum week implements A{
         mon,tues,wed,thur,fri,sat,sun;
+        //these are enum constant
+        //public ,static and final
+        //since its final we can create child enums
         //type of week
 week(){
     System.out.println("Constructor for "+ this);
