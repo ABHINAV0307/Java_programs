469d64a719b535c4050b001c1bb43cfe29e64589
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2022-12-29 21:44:51 +0530

    Create main.java

---
+++ OOPs/interfaces/main.java
@@ -0,0 +1,12 @@
+package com.abhi.OOPs.interfaces;
+
+public class main {
+    public static void main(String[] args) {
+//        car car =new car();
+//        car.acc();
+//        car.start();
+//        car.stop();
+        nicecar car=new nicecar();
+car.start();
+    }
+}
