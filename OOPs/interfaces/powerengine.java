6cac77e57a36ccef640f6ad90ecbc0822b02b78d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-01-02 09:38:03 +0530

    Create powerengine.java

---
+++ OOPs/interfaces/powerengine.java
@@ -0,0 +1,12 @@
+package com.abhi.OOPs.interfaces;
+
+public class powerengine implements engine{
+
+
+    @Override
+    public void start() {
+        System.out.println("Engine start");
+    }
+
+    @Override
+    public void stop() {
