7ee7ae6ba6f0e230c40d68ebed87b26a4f096fa4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-02 00:52:27 +0530

    Update valid_parenthesis_20.java

--- leetcode/valid_parenthesis_20.java
+++ leetcode/valid_parenthesis_20.java
@@ -4,3 +4,7 @@ public class valid_parenthesis_20 {
     class Solution {
         public boolean isValid(String str) {
             int a=0,b=0,c=0;
+            int n=str.length();
+            char[] s = str.toCharArray();
+            for(int i=0;i<n;i=i++)
+            {
