066dfd7b420dde88eaeee9d108e2a33843e53af0
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-03 00:13:02 +0530

    Update basics.java

--- Hashset/basics.java
+++ Hashset/basics.java
@@ -5,3 +5,8 @@ import java.util.Map;
 
 public class basics {
     public static void main(String[] args) {
+        HashMap<String,Integer> map =new HashMap<>();
+        map.put("India",120);
+        map.put("China",150);
+        map.put("US",30);
+        System.out.println(map);
