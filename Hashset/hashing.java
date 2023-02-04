908315d00bba3689799a1e50ec24af3f1400c00d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-04 06:53:59 +0530

    Create hashing.java

---
+++ Hashset/hashing.java
@@ -0,0 +1,8 @@
+package com.abhi.Hashset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class hashing {
+    public static void main(String[] args) {
+        HashSet<Integer> set =new HashSet<>();
