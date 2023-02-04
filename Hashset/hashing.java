c31da7d6941c395cf22d0529d58116df2b4976e8
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-04 06:54:06 +0530

    Update hashing.java

--- Hashset/hashing.java
+++ Hashset/hashing.java
@@ -6,6 +6,14 @@ import java.util.Iterator;
 public class hashing {
     public static void main(String[] args) {
         HashSet<Integer> set =new HashSet<>();
+        set.add(5);
+        set.add(1);
+        System.out.println(set);
+        //set is unique but need not be in order
+//another way of traversal using iterator
+        Iterator it= set.iterator();
+while(it.hasNext())
+    System.out.println(it.next());
 
         }
     }
