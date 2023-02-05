5f228d3cee430e23937286ea84b52ac8546d92f1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-05 21:53:48 +0530

    Update valid_parenthesis_20.java

--- leetcode/valid_parenthesis_20.java
+++ leetcode/valid_parenthesis_20.java
@@ -28,3 +28,12 @@ public class valid_parenthesis_20 {
                 for(int i=0;i<n;i=i+2)
         {
         if(s[i]=='(' && s[i+1]==')')
+        flag=1;
+
+        else if(s[i]=='{' && s[i+1]=='}')
+        flag=1;
+
+        else if(s[i]=='[' && s[i+1]==']')
+        flag=1;
+
+         else
