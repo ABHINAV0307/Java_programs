b3cbe874e1ec71fb56a6c6abc389c563b1e9e776
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-08 00:10:52 +0530

    Update code1.java

--- placement_preparation_class/code1.java
+++ placement_preparation_class/code1.java
@@ -3,6 +3,11 @@ package com.abhi.placement_preparation_class;
 import java.util.Scanner;
 
 public class code1
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int mno=sc.nextInt();
+        int r=sc.nextInt();
             for(int i=0;i<mno;i++){
 
             }
