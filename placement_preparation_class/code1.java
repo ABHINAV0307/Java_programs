b8a6c231b37ddd191cb81ad6225de150a632c677
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-08 00:10:57 +0530

    Update code1.java

--- placement_preparation_class/code1.java
+++ placement_preparation_class/code1.java
@@ -8,6 +8,11 @@ public class code1
         Scanner sc=new Scanner(System.in);
         int mno=sc.nextInt();
         int r=sc.nextInt();
+        int fno=sc.nextInt();
+        for(int i=0;i<mno;i++){
+
+        }
+        if(r<=(mno+fno)&&mno>fno){
             for(int i=0;i<mno;i++){
 
             }
