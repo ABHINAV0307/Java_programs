e904c7dbe7d64ba1342d220493c59acdf8794fc5
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-09 14:44:56 +0530

    Update calculator.java

--- Basics/calculator.java
+++ Basics/calculator.java
@@ -1,12 +1,3 @@
-                    System.out.println(a - b);
-                else if (ch == '/'&& b!=0)
-                    System.out.println(a / b);
-                else if (ch == '*')
-                    System.out.println(a * b);
-                else
-                    System.out.println(a % 10);
-
-            System.out.println("\t\t\tNEW CALCULATION");
         }
 
     }
