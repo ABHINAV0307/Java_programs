9fd2be3f6f6347c73d4160d873fc0f5abff7339b
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-12 20:41:15 +0530

    Update student.java

--- placement_preparation_class/student.java
+++ placement_preparation_class/student.java
@@ -5,3 +5,8 @@ import java.util.Scanner;
 public class student {
 long mark;
 String name,address,branch;
+
+    public static void main(String[] args) {
+    student st1=new student();
+        student st2=new student();
+        Scanner sc;
