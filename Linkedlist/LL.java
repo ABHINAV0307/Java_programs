927fe2824f683fdeaf32281c22ea5ea6dbc92157
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-16 23:38:40 +0530

    Update LL.java

--- Linkedlist/LL.java
+++ Linkedlist/LL.java
@@ -35,4 +35,9 @@ private Node head;
         private Node next;
 
         public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
 }
