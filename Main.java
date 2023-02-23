d95578a356d8c265097f1b4cc186d5300b811b9f
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-24 00:08:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,3 @@
-package com.abhi;
-
-import java.util.Scanner;
-
-public class Main {
-public static void main(String[] args){
-    //basic output
-    System.out.println("Hello World");
     //basic input
     Scanner input=new Scanner(System.in);
     System.out.println(input.nextInt());
