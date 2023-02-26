1f63d7198cba9b23aed604e30f1502e94c9b8c82
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-26 22:42:55 +0530

    Create a2b.java

---
+++ Bit_Manipulation/a2b.java
@@ -0,0 +1,7 @@
+//package com.abhi.Bit_Manipulation;
+//
+//import java.util.Scanner;
+//
+//public class a2b {
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
