b3af10cc6803a2b8f2aca58d775f2b5c4e365ae4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-02-26 22:43:02 +0530

    Update a2b.java

--- Bit_Manipulation/a2b.java
+++ Bit_Manipulation/a2b.java
@@ -5,3 +5,10 @@
 //public class a2b {
 //    public static void main(String[] args) {
 //        Scanner sc =new Scanner(System.in);
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        while(b&&a){
+//            if()
+//        }
+//    }
+//}
