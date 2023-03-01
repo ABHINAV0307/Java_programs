853e72d808b27eef5402babcd3e6fc68b809ec2e
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-01 12:19:04 +0530

    Update flip.java

--- Bit_Manipulation/flip.java
+++ Bit_Manipulation/flip.java
@@ -5,3 +5,10 @@ public class flip {
         for(int []row:image ){
             for(int i=0;i<(image.length+1)/2;i++){
                 int temp=row[i]^1;
+                row[i]=row[image[0].length-i-1]^1;
+                row[image[0].length-i-1]=temp;
+            }
+        }
+return image;
+    }
+}
