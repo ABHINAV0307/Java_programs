2a2a0c4b45ff8e4d365994658c3107b18a05d2b9
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-02 09:19:45 +0530

    Create Magic_number.java

---
+++ Bit_Manipulation/Magic_number.java
@@ -0,0 +1,9 @@
+package com.abhi.Bit_Manipulation;
+
+public class Magic_number {
+    public static void main(String[] args) {
+        int a=6;
+        int sum=0;
+        int base=5;
+        while(a>0){
+            int last=a&1;
