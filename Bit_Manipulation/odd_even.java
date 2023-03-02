0b9108b780ad872f6a9b1af268d68549af16de57
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-02 09:20:19 +0530

    Update odd_even.java

--- Bit_Manipulation/odd_even.java
+++ Bit_Manipulation/odd_even.java
@@ -5,3 +5,11 @@ import java.util.Scanner;
 public class odd_even {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a&1)==1){
+            System.out.println("odd");
+        }
+        else
+            System.out.println("even");
+    }
+}
