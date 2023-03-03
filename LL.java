8cc5e8a47977cf651e722123165b230c681cac94
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-03 09:42:05 +0530

    Create LL.java

---
+++ LL.java
@@ -0,0 +1,13 @@
+//package com.abhi.Linkedlist;
+//
+//public class Linked {
+//private Node head;
+//    private Node tail;
+//    private int size;
+//
+//    LL(){
+//        this.size=0;
+//    }
+//
+//
+//    public void insertFirst(int val){
