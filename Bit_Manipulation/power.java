fa05cb237ff1561faab3a326dc7f17c0d77dd1ab
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-03 09:41:52 +0530

    Create power.java

---
+++ Bit_Manipulation/power.java
@@ -0,0 +1,8 @@
+package com.abhi.Bit_Manipulation;
+
+public class power {
+    public static void main(String[] args) {
+        int power=6;
+        int base=3;
+        int ans=1;
+        while(power>0){
