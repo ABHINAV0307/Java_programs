331a3d0605109096385d024a1f36cf0246f48cc2
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-03 09:41:55 +0530

    Update power.java

--- Bit_Manipulation/power.java
+++ Bit_Manipulation/power.java
@@ -6,3 +6,12 @@ public class power {
         int base=3;
         int ans=1;
         while(power>0){
+            if((power&1)==1){
+     ans*=base;
+            }
+            power=power>>1;
+            base*=base;
+        }
+        System.out.println(ans);
+    }
+}
