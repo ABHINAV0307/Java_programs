be8b5a1d6eeac89d35a137e68722d8543aaa6092
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-04 10:31:21 +0530

    Update xor.java

--- Bit_Manipulation/xor.java
+++ Bit_Manipulation/xor.java
@@ -6,3 +6,17 @@ public class xor {
 
         int a = 4;
         int b = 9;
+        int res=xor(b)^xor(a-1);
+        System.out.println(res);
+    }
+    static int xor(int n){
+        if(n%4==0)
+            return n;
+        if(n%4==1)
+            return 1;
+        if(n%4==2)
+            return n+1;
+
+        return 0;
+    }
+}
