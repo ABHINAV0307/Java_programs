e507515eac5abc66c67bcd9372dc0e55f68eb4ed
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-04 10:31:26 +0530

    Create LastString.java

---
+++ leetcode/LastString.java
@@ -0,0 +1,8 @@
+package com.abhi.leetcode;
+
+public class LastString {
+    public static void main(String[] args) {
+        String str = "   fly me   to   the moon     ";
+
+        int ans = 0;
+        String[] s = str.split(" ");
