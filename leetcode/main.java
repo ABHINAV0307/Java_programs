f367ff33053ec208e2258b72e08e59d140d6a364
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-04 18:45:45 +0530

    Create main.java

---
+++ leetcode/main.java
@@ -0,0 +1,9 @@
+package com.abhi.leetcode;
+
+public class main {
+    public static void main(String[] args) {
+        String a="ab";
+        String b="cd";
+        String c="acbd";
+
+        char[] ch1 = a.toCharArray();
