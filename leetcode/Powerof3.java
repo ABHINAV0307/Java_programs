d05b81536b81666223cab31fe2b638acc53bbf1f
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-05 07:16:02 +0530

    Create Powerof3.java

---
+++ leetcode/Powerof3.java
@@ -0,0 +1,8 @@
+package com.abhi.leetcode;
+
+import java.util.Scanner;
+
+public class Powerof3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
