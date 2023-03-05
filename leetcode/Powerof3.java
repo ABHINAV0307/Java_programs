479754ea324936244fb354e5923622433c7bcf0e
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-05 07:16:05 +0530

    Update Powerof3.java

--- leetcode/Powerof3.java
+++ leetcode/Powerof3.java
@@ -6,3 +6,12 @@ public class Powerof3 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        double val=  (Math.log(n)/Math.log(3));
+        int newval= (int) Math.pow(3,val);
+        if(newval==n)
+            System.out.println("True");
+        else
+            System.out.println("False");
+
+    }
+}
