a7072bbc40d0c839d93f833b19cb919257464568
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-05 07:15:53 +0530

    Create powerof2.java

---
+++ leetcode/powerof2.java
@@ -0,0 +1,8 @@
+package com.abhi.leetcode;
+
+public class powerof2 {
+    public static void main(String[] args) {
+        int val = 65498;
+        //[1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+        int sum = 0;
+        int base = 1;
