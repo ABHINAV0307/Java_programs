be435b598b191cde255cc711e66d97a98d045c68
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-14 22:34:37 +0530

    Create ques.java

---
+++ leetcode/ques.java
@@ -0,0 +1,11 @@
+package com.abhi.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ques {
+    public static void main(String[] args) {
+        int [] a={1,2,3,4,12,42};
+        Arrays.sort(a);
+        for(int i=0;i<a.length;i++) {
+            System.out.print(a[i]+ "  ");
