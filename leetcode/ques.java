861d6a93af6768c3b6941057881713c69e8f733f
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-14 22:34:40 +0530

    Update ques.java

--- leetcode/ques.java
+++ leetcode/ques.java
@@ -9,3 +9,15 @@ public class ques {
         Arrays.sort(a);
         for(int i=0;i<a.length;i++) {
             System.out.print(a[i]+ "  ");
+        }
+        int k=0;
+        System.out.println(" ");
+        for(int i=0;i<a.length-1;i++) {
+            if (a[i] == a[i+1]) {
+                System.out.print(a[i]+" *");
+                System.out.print(a[i+1]+"# ");
+                //System.out.println("Repeat");
+            }
+        }
+    }
+}
