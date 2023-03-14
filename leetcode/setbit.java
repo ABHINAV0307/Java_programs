77840c6fc49016dae73ecdaa8268f6415f5e1cc1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-14 22:34:57 +0530

    Update setbit.java

--- leetcode/setbit.java
+++ leetcode/setbit.java
@@ -3,3 +3,7 @@ package com.abhi.leetcode;
 public class setbit {
     public static void main(String[] args) {
         int a=8;
+        double l=  Math.log(a)/Math.log(2);
+        System.out.println(l);
+    }
+}
