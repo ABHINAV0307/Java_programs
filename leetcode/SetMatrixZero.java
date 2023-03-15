309dc182b75de1c71936827208df110a6ec0600d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-15 21:56:54 +0530

    Update SetMatrixZero.java

--- leetcode/SetMatrixZero.java
+++ leetcode/SetMatrixZero.java
@@ -7,6 +7,14 @@ public class SetMatrixZero {
         char[] ch = s.toCharArray();
         int a = 0;
         int k = 0;
+        int b = str.length() - 1;
+
+        while (a < b) {
+            if ((ch[a] < 'A' || ch[a] > 'Z')) {
+                a++;
+            } else if ((ch[b] < 'A' || ch[b] > 'Z')) {
+                b--;
+            } else if (ch[a] != ch[b]) {
                 k++;
             }
             a++;
