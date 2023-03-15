95f1f4901a42b10e3c90f0a0ee485788eeff7518
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-15 21:57:58 +0530

    Create sumpair.java

---
+++ leetcode/sumpair.java
@@ -0,0 +1,16 @@
+package com.abhi.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sumpair {
+    public static void main(String[] args) {
+        int n=6;
+        int k=3;
+        int count=0;
+        List<Integer> arr = new ArrayList<Integer>(5);
+        arr.add(1);
+        arr.add(3);
+        arr.add(2);
+        arr.add(6);
+        arr.add(1);
