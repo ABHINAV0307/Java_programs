e77605112f0868bf49b440e8d5a58108d41b5e4d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-15 21:56:46 +0530

    Create SetMatrixZero.java

---
+++ leetcode/SetMatrixZero.java
@@ -0,0 +1,9 @@
+package com.abhi.leetcode;
+
+public class SetMatrixZero {
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        String s = str.toUpperCase();
+        char[] ch = s.toCharArray();
+        int a = 0;
+        int k = 0;
