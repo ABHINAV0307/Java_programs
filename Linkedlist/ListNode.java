0cba013b0a11de3bee3972f08eba40a65cae7a6e
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-16 22:13:05 +0530

    Update ListNode.java

--- Linkedlist/ListNode.java
+++ Linkedlist/ListNode.java
@@ -17,6 +17,17 @@ public class ListNode {
 
                 else if(temp.next!=null){
                     if(temp.next.next==null&&temp.next.val==val){
+                        temp.next=null;
+                    }
+                    else if((temp.val==val)){
+                        temp.val=temp.next.val;
+                        temp.next=temp.next.next;
+                        if(temp.val==val){
+                            temp.val=temp.next.val;
+                            temp.next=temp.next.next;
+                        }
+                    }
+                }
 
                 temp=temp.next;
 
