933d18d60ee471f4ec8f91b41160fc978364a170
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-21 20:20:22 +0530

    Update arr.java

--- leetcode/arr.java
+++ leetcode/arr.java
@@ -3,6 +3,11 @@ package com.abhi.leetcode;
 import java.util.Arrays;
 
 public class arr {
+    public static void main(String[] args) {
+        int []arr=new int[]{1,0,0,0,1};
+                Arrays.sort(arr);
+                for(int i=0;i<arr.length-1;i++){
+                    if((arr[i]+1)!=(arr[i+1])){
                         System.out.println(arr[i]+1);
                     }
         }
