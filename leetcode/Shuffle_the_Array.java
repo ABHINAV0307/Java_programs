964e04e578cd6faaa7a4b5392a56edbae5c6266c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-22 23:47:16 +0530

    Update Shuffle_the_Array.java

--- leetcode/Shuffle_the_Array.java
+++ leetcode/Shuffle_the_Array.java
@@ -1,6 +1,10 @@
 package com.abhi.leetcode;
 
 import java.util.Arrays;
+
+public class Shuffle_the_Array {
+    public static void main(String[] args) {
+        int []arr=new int[]{1,0,0,0,1};
         }
     }
 }
