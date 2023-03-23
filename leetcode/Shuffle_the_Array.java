b32fc2f93c6e6abc5bebc9145b3c07f8a8a11054
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-23 08:45:43 +0530

    Update Shuffle_the_Array.java

--- leetcode/Shuffle_the_Array.java
+++ leetcode/Shuffle_the_Array.java
@@ -8,6 +8,9 @@ public class Shuffle_the_Array {
         int []a=new int [5];
        int jk=0;
         for(int i=0;i<arr.length-1;i++){
+            if((arr[i]+1)!=(arr[i+1])){
+                System.out.println(arr[i]+1);
+            }
         }
     }
 }
