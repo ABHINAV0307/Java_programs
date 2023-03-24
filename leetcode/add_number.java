163ed1a12aba6e787322023bd56e8343ce2fc82d
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-24 22:27:05 +0530

    Update add_number.java

--- leetcode/add_number.java
+++ leetcode/add_number.java
@@ -5,3 +5,9 @@ public class add_number {
         int num=38;
         while(num>9){
             int rem=0;
+            }
+            num=sum;
+        }
+        System.out.println(num);
+    }
+}
