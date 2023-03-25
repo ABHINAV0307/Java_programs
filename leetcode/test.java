a042cd408465c3525f8d7c069c2e05d4d6871d3e
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-25 07:38:19 +0530

    Update test.java

--- leetcode/test.java
+++ leetcode/test.java
@@ -4,6 +4,15 @@ public class test {
     public static void main(String[] args) {
         int num=38;
         int val=num;
+        while(val>9){
+num=val;
+        int sum = 0;
+        int rem = 0;
+        while(num > 0) {
+            rem = num % 10;
+            sum += rem;
+            num = num / 10;
+        }
 
         val=sum;
 
