f97b19639cf28695a4f910efed51f197f3ca7a0f
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-03-30 23:22:27 +0530

    Update StackDemo.java

--- Stack/StackDemo.java
+++ Stack/StackDemo.java
@@ -2,3 +2,7 @@ package com.abhi.Stack;
 
 import java.util.*;
 
+        public class StackDemo {
+            public static void main(String args[])
+            {
+                // Creating an empty Stack
