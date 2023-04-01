79e97f2472f7e6aa3e947729fdcfe5efc198b716
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-04-01 22:20:34 +0530

    Update Truncate_Sentence.java

--- leetcode/Truncate_Sentence.java
+++ leetcode/Truncate_Sentence.java
@@ -8,3 +8,7 @@ public class Truncate_Sentence {
     public static void main(String[] args) {
         String s="Hello how are you Contestant";
         int k=4;
+        String[] str=s.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i <4; i++) {
+            sb.append(str[i]+" ");
