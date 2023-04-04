e4f23b6494fe58d39e1329a381dfefc0b642aab1
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-04-04 21:46:53 +0530

    Update unique.java

--- leetcode/unique.java
+++ leetcode/unique.java
@@ -2,3 +2,6 @@ package com.abhi.leetcode;
 
 public class unique {
     public static void main(String[] args) {
+        int []arr=new int [26];
+        String s="aabb";
+        char[]ch=s.toCharArray();
