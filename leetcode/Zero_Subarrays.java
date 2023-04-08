1f903d9ab7466c95534d1aa8c5c18a14a7e9c4d4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-04-09 00:26:31 +0530

    Update Zero_Subarrays.java

--- leetcode/Zero_Subarrays.java
+++ leetcode/Zero_Subarrays.java
@@ -3,3 +3,5 @@ package com.abhi.leetcode;
 public class Zero_Subarrays {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 3, 0, 0, 2, 0, 0, 4};
+        long ans = 0, no = 0;
+        for (int i : nums) {
