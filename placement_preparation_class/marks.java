eae680c99757df31836abf4e7ee160a0d57c8e5c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-04-24 00:53:51 +0530

    Update marks.java

--- placement_preparation_class/marks.java
+++ placement_preparation_class/marks.java
@@ -3,4 +3,6 @@ package com.abhi.placement_preparation_class;
 public class marks {
     public static void main(String[] args) {
         int []arr=new int[]{9,9};
+        int l=arr.length;
+        int val=1;
 }
