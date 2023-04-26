8cfca826396292de176dcfbc6100cc6be0758b93
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-04-26 14:09:59 +0530

    Delete ListNode.java

--- Linkedlist/ListNode.java
+++ Linkedlist/ListNode.java
@@ -1,6 +1,3 @@
-          int val;
-          ListNode next;
-          ListNode() {}
           ListNode(int val) { this.val = val; }
           ListNode(int val, ListNode next) { this.val = val; this.next = next; }
       }
