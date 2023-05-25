00b29d997b778a96688b8513cd3a2ccd813c5c4a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-05-25 23:42:26 +0530

    Update main.java

--- Linkedlist/main.java
+++ Linkedlist/main.java
@@ -1,9 +1,6 @@
 package com.abhi.Linkedlist;
 
 public class main {
-    public static void main(String[] args) {
-        LinkedList<String> list = new LinkedList<String>();
-        list.add("A");
         list.add("B");
         list.add("C");
         list.add("D");
