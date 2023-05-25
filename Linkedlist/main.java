185cf0e3b300714ffcdfeabb0ab6e65a4cce449f
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-05-25 23:42:22 +0530

    Update main.java

--- Linkedlist/main.java
+++ Linkedlist/main.java
@@ -1,8 +1,5 @@
 package com.abhi.Linkedlist;
 
-import java.util.LinkedList;
-//using collection framework
-
 public class main {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<String>();
