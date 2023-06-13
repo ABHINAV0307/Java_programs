c257aed2cab70efb42c210c710d5fb78984508c3
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-13 23:55:16 +0530

    Create all_path.java

---
+++ Backtracking/all_path.java
@@ -0,0 +1,7 @@
+package com.abhi.Backtracking;
+
+public class all_path {
+    public static void main(String[] args) {
+        boolean [][] board={
+                {true,true,true},
+                {true,true,true},
