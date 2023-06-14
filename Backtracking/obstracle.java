cc9e1e9c6b473066c2be0c3fd149aa82b61b0b66
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-15 00:15:18 +0530

    Update obstracle.java

--- Backtracking/obstracle.java
+++ Backtracking/obstracle.java
@@ -3,3 +3,10 @@ package com.abhi.Backtracking;
 public class obstracle {
     public static void main(String[] args) {
 boolean [][] board={
+        {true,true,true},
+        {true,false,true},
+        {true,true,true}
+};
+    path("",board,0,0);
+    }
+    static void path(String p ,boolean[][] maze,int r,int c){
