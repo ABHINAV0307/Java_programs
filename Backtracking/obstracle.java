92431cd9b0b4a42f14e8858ab3555c06e64ab270
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-15 00:15:21 +0530

    Update obstracle.java

--- Backtracking/obstracle.java
+++ Backtracking/obstracle.java
@@ -10,3 +10,13 @@ boolean [][] board={
     path("",board,0,0);
     }
     static void path(String p ,boolean[][] maze,int r,int c){
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r<maze.length-1 ){
+            path(p+"D",maze,r+1,c);
+        }
+        //note there is r+1 and c+1 as we are moving from 0 to length
