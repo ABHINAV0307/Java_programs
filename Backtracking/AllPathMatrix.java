795ba07cefad2efa3a23760aa72838b41bd78813
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-16 09:45:59 +0530

    Update AllPathMatrix.java

--- Backtracking/AllPathMatrix.java
+++ Backtracking/AllPathMatrix.java
@@ -11,3 +11,8 @@ public class AllPathMatrix {
         };
         int [][] path=new int [board.length][board[0].length];
         pathprint("",board,0,0,path,1);
+    }
+    static void pathprint(String p ,boolean[][] maze,int r,int c,int [][]path,int step){
+        if(r==maze.length-1 && c== maze[0].length - 1){
+            path[r][c]=step;
+            for(int []arr : path){
