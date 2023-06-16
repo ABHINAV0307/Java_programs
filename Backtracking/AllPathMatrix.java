710c6c2d1aea24988e915ea348ebca7c734f6869
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-16 09:45:54 +0530

    Update AllPathMatrix.java

--- Backtracking/AllPathMatrix.java
+++ Backtracking/AllPathMatrix.java
@@ -5,3 +5,9 @@ import java.util.Arrays;
 public class AllPathMatrix {
     public static void main(String[] args) {
         boolean [][] board={
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        int [][] path=new int [board.length][board[0].length];
+        pathprint("",board,0,0,path,1);
