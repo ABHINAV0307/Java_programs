e7319e8b577f4ca36b9676b99130356a66100fa4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-17 22:33:50 +0530

    Update AllPathMatrix.java

--- Backtracking/AllPathMatrix.java
+++ Backtracking/AllPathMatrix.java
@@ -16,3 +16,7 @@ public class AllPathMatrix {
         if(r==maze.length-1 && c== maze[0].length - 1){
             path[r][c]=step;
             for(int []arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
