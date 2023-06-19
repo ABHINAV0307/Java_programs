b7f4a25ef3b76429039983c31b383d56b0f3c41b
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-20 01:56:52 +0530

    Update maze_count.java

--- Backtracking/maze_count.java
+++ Backtracking/maze_count.java
@@ -5,3 +5,11 @@ public class maze_count {
         System.out.println(count(3,3));
     }
     static int count(int r, int c){
+        if(r==1 || c==1){
+            return 1;
+        }
+        int left=count(r-1,c);
+        int right=count(r,c-1);
+    return left+right;
+    }
+}
