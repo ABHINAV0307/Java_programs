a29ad7b4f277313e3ba4d1a296b72b5e51a3bc9c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-19 19:49:31 +0530

    Update diagonal.java

--- Backtracking/diagonal.java
+++ Backtracking/diagonal.java
@@ -3,6 +3,13 @@ package com.abhi.Backtracking;
 import java.util.ArrayList;
 
 public class diagonal {
+    static ArrayList<String> pathlist(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
         if(c>1){
             list.addAll(pathlist(p+"L",r,c-1));
         }
