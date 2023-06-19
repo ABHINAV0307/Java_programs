872196d73fca09524b3abd9ea7af80d725936e51
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-19 19:49:25 +0530

    Update diagonal.java

--- Backtracking/diagonal.java
+++ Backtracking/diagonal.java
@@ -3,3 +3,9 @@ package com.abhi.Backtracking;
 import java.util.ArrayList;
 
 public class diagonal {
+        if(c>1){
+            list.addAll(pathlist(p+"L",r,c-1));
+        }
+        return list;
+    }
+}
