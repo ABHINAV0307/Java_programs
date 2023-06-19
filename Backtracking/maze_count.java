2c916d9e4ff843dae4998103bb7f38167f1d741a
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-20 01:56:48 +0530

    Create maze_count.java

---
+++ Backtracking/maze_count.java
@@ -0,0 +1,7 @@
+package com.abhi.Backtracking;
+
+public class maze_count {
+    public static void main(String[] args) {
+        System.out.println(count(3,3));
+    }
+    static int count(int r, int c){
