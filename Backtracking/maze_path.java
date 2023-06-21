96c20a6b0b1eb89986486b7eaa36a5699e0f3df3
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-21 23:48:01 +0530

    Update maze_path.java

--- Backtracking/maze_path.java
+++ Backtracking/maze_path.java
@@ -4,3 +4,10 @@ import java.util.ArrayList;
 
 public class maze_path {
     public static void main(String[] args) {
+        }
+        if(c>1 && r!=1 && c!=1){
+            list.addAll(pathlist(p+"L",r,c-1));
+        }
+        return list;
+    }
+}
