c2b0d782faa5e7207643e4b179de3b7a0caf17c4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-06-24 02:15:58 +0530

    Update obstracles_allpath.java

--- Backtracking/obstracles_allpath.java
+++ Backtracking/obstracles_allpath.java
@@ -4,3 +4,10 @@ public class obstracles_allpath {
     public static void main(String[] args) {
 
     }
+        }
+        //note there is r+1 and c+1 as we are moving from 0 to length
+        if(c<maze[0].length-1){
+            path(p+"L",maze,r,c+1);
+        }
+    }
+}
