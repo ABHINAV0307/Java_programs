f7a2651c4f54828cb21f2b987a82769911f80b72
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-07-05 21:23:07 +0530

    Update LL.java

--- Linkedlist/LL.java
+++ Linkedlist/LL.java
@@ -56,6 +56,11 @@ public class ll {
     private Node insertRec(int val, int index, Node node) {
         if (index == 0) {
             Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
 
+        node.next = insertRec(val, index-1, node.next);
+         return node;
     }
 }
