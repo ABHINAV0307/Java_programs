ca88d16d7a26ae446ea65f2e1183f72018b260c9
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-07-20 23:57:27 +0530

    Update main.java

--- Linkedlist/main.java
+++ Linkedlist/main.java
@@ -1,4 +1,8 @@
 package com.abhi.Linkedlist;
 
 public class main {
-}
+        public static void main(String[] args) {
+            ll list = new ll();
+            list.addfirst(3);
+            list.addfirst(2);
+            list.addfirst(8);
