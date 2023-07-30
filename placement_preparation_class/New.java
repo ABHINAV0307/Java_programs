0572d9f5d720fa47ca93f689f5c16c9f585e81f4
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-07-30 10:34:12 +0530

    Create New.java

---
+++ placement_preparation_class/New.java
@@ -0,0 +1,6 @@
+//package com.abhi.placement_preparation_class;
+//
+//public class New {
+//    public static void main(String[] args) {
+//        int [] spells=new int[]{3,1,2};
+//        int [] potions=new int[]{8,5,8};
