b2c22dbb6509d4762568be00232726db2c64fd9c
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-01 12:04:41 +0530

    Update newtest.java

--- placement_preparation_class/newtest.java
+++ placement_preparation_class/newtest.java
@@ -5,6 +5,13 @@ public class newtest {
         int [] nums = new int []{1,7,3,6,5,6};
         int [] n = new int [nums.length];
         n[0]=nums[0];
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+           max+=nums[i];
+        }
+        for(int i=1;i<nums.length;i++){
+            n[i]=nums[i]+n[i-1];
+        }
         }
         }
 
