365c4630a36849c259f7a8623b1f95676e14c1bd
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-01 12:04:32 +0530

    Create newtest.java

---
+++ placement_preparation_class/newtest.java
@@ -0,0 +1,7 @@
+package com.abhi.placement_preparation_class;
+
+public class newtest {
+    public static void main(String[] args) {
+        int [] nums = new int []{1,7,3,6,5,6};
+        int [] n = new int [nums.length];
+        n[0]=nums[0];
