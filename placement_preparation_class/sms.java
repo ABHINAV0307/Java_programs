c3b7162413512ba0ac838f922322128329615c00
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-05 10:39:51 +0530

    Update sms.java

--- placement_preparation_class/sms.java
+++ placement_preparation_class/sms.java
@@ -3,6 +3,10 @@ package com.abhi.placement_preparation_class;
 public class sms {
     String name;
     int marks;
+    static int rollno=2;
+
+    public static void main(String[] args) {
+        sms ob1=new sms();
       //  System.out.println(ob1.rollno);
 
         System.out.println(ob2.rollno);
