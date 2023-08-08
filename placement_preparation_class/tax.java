400a8421ea2c524b7473384a42637c22b270cb70
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-08 23:16:00 +0530

    Create tax.java

---
+++ placement_preparation_class/tax.java
@@ -0,0 +1,7 @@
+package com.abhi.placement_preparation_class;
+
+import java.util.Scanner;
+//jiske aage parenthesis hota hai voh method hota hai / also called function
+public class tax {
+    private int sal;
+    static int incometax(int sal) {
