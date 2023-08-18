d0e266d6eb3ea7b4ecfd1a5cf6d0f0053edb8603
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-18 23:40:07 +0530

    Update test.java

--- placement_preparation_class/test.java
+++ placement_preparation_class/test.java
@@ -4,3 +4,8 @@ public class test {
     public static void main(String[] args) {
 
         int n=4;
+        int []ltr=new int [n];
+        int []rtl=new int [n];
+        ltr[0]=1;
+        rtl[n-1]=n;
+        for(int i=1;i<n;i++){
