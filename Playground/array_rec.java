7c746b1900e692517629256a6d5a75e91a9fb460
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-19 12:27:02 +0530

    Update array_rec.java

--- Playground/array_rec.java
+++ Playground/array_rec.java
@@ -4,3 +4,11 @@ import java.util.Arrays;
 
 public class array_rec {
     public static void main(String[] args) {
+        int []arr=new int[]{1,2,3,4,5,7,8,9,10,11};
+    int fsmall=Integer.MAX_VALUE;
+    int ssmall=Integer.MAX_VALUE;
+
+    int fbig=Integer.MIN_VALUE;
+    int sbig=Integer.MIN_VALUE;
+
+
