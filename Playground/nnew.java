082fc686e12eac03350b45bc48b69e3e92f25520
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-24 14:40:35 +0530

    Update nnew.java

--- Playground/nnew.java
+++ Playground/nnew.java
@@ -13,3 +13,11 @@ public class nnew {
 //        System.out.print(print(0));
 //        System.out.println();
 //        System.out.print(print(1));
+//        System.out.println();
+//        System.out.print(print(3));
+//        System.out.println();
+
+
+        int []arr=new int []{1,1,2,2,2,3,4,5,5,5};
+        ArrayList<Integer> val=new ArrayList<>();
+        val.add(2);
