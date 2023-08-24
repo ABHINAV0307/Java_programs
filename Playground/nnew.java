a2f554dba28078a4ef3919e0c2fff57c04a99abb
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-08-24 14:40:28 +0530

    Update nnew.java

--- Playground/nnew.java
+++ Playground/nnew.java
@@ -5,3 +5,11 @@ import java.util.HashSet;
 
 public class nnew {
     public static void main(String[] args) {
+        // fibonacci number
+//        for(int i=0;i<40;i++)
+//        {
+//            System.out.print(print(i) +"-->");
+//        }
+//        System.out.print(print(0));
+//        System.out.println();
+//        System.out.print(print(1));
