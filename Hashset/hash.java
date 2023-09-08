47a9b31d03d34059328a6997241cc19f5b4995fa
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-09-08 09:33:07 +0530

    Update hash.java

--- Hashset/hash.java
+++ Hashset/hash.java
@@ -5,5 +5,8 @@ import java.util.Map;
 
 //internal implementations
 public class hash {
-    
-}
+    public static void main(String[] args) {
+        int [] arr=new int[]{1,1,2,3,4,5,4,5};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        {
