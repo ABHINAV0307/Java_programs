2a13506c833f2bc89bcd4c71d58f72862a4e82df
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-09-08 09:33:12 +0530

    Update hash.java

--- Hashset/hash.java
+++ Hashset/hash.java
@@ -10,3 +10,8 @@ public class hash {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         {
+            for (int i = 0; i < arr.length;i++){
+                if(map.containsKey(arr[i])){
+                    map.put(arr[i],map.get(arr[i])+1);
+                }else
+                    map.put(arr[i],1);
