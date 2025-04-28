c47edcc03ee82d820eb8b4116383c030dba9974b
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2025-04-28 21:49:36 +0530

    Files addition

--- Playground/sept.java
+++ Playground/sept.java
@@ -4,14 +4,17 @@ public class sept {
 }
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        int deec=0,inc=0;
+        int dec=0,inc=0;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]>=nums[i+1]){
                 dec++;
             }
             if(nums[i]<=nums[i+1])
-                inc++
+                inc++;
         }
         if(inc==nums.length-1 || dec == nums.length-1){
             return true;
         }
+        return false;
+    }
+}
--- out/production/Java_programs/com/abhi/README.md
+++ out/production/Java_programs/com/abhi/README.md
@@ -1,3 +1,2 @@
 #Java_program
 This include most of the java programs from basic to advance
-
